ae69c05b8f65d9aeb0a392e50c7c847fe517b774
namwoody <deepsarora262@gmail.com>
2015-09-19 08:13:36 -0400

    done

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
+// database
 /**#@+
  * Authentication Unique Keys and Salts.
  *
